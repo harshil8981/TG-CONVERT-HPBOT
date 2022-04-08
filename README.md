0c6d6edda31b320913a4769254db13d37255c58d
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2022-04-08 18:21:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,83 @@
+## TG-CONVERTER-HPBOT 
+This repo is the clone of [Hp_converterbot](https://telegram.dog/Hp_converterbot) there is more difference between my bot and this repo. But the purposes is same
+
+
+[![Open Source? Yes!](https://badgen.net/badge/Open%20Source%20%3F/Yes%21/blue?icon=github)](https://github.com/harshil8981/TG-CONVERT-HPBOT/tree/main)
+[![Ask Me Anything !](https://img.shields.io/badge/Ask%20me-anything-1abc9c.svg)](https://telegram.dog/Hp_botupdate)
+
+## What is the difference b/w master and beta?
+
+- [X] Added password for using bot as private 
+- [X] Added database for permanent thumbnail 
+
+<u> Note: </u> If you find any bug report in [Hp bot Support](https://telegram.dog/Hp_botupdate)
+
+
+## How to create your own bot
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/harshil8981/TG-CONVERT-HPBOT/tree/beta)
+- 👆 Press the deploy button.
+
+- Give a name to your app.
+
+- Get your API hashes, Id from [my.telegram.org](https://my.telegram.org/)
+
+- Paste the below API_HASH and API_ID respectively.
+
+- Get the Bot Father Token from [@BotFather](https://telegram.dog/botfather)
+
+- Paste the token below TG_BOT_TOKEN.
+
+- Give your username in USER_ID.
+
+<i>YouTube video</i> - <a href="https://youtu.be/zQamSjXBpJU"><img src="https://img.shields.io/badge/How%20To-Create-red.svg?logo=Youtube"></a>
+
+
+## How to reach Me ?
+<a href="https://telegram.dog/Hp_botupdate"><img src="https://img.shields.io/badge/Join-Telegram%20Channel-red.svg?logo=Telegram"></a>
+<a href="https://telegram.dog/Hp_botupdate"><img src="https://img.shields.io/badge/Join-Telegram%20Group-blue.svg?logo=telegram"></a>
+
+## COMMANDS
+### AVAILABLE COMMANDS 
+```
+start - check whether bot is alive 
+
+converttovideo - reply to the media for converting into video 
+
+converttofile - reply to the media for converting into file 
+
+help - know how to use this bot
+
+about - to know about me
+```
+### start
+<p align="center">
+<img src="https://telegra.ph/file/3cb7e3725cd991373dd9b.jpg" alt="START">
+
+### help
+<p align="center">
+<img src="https://telegra.ph/file/0ba25cedf094225ad7175.jpg" alt="HELP">
+
+### about
+<p align="center">
+<img src="https://telegra.ph/file/f6ad0e22825afe65ddcd2.jpg" alt="ABOUT">
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+#
+#
+![](https://raw.githubusercontent.com/acervenky/animated-github-badges/master/assets/pro.gif)
+
+## License
+[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://github.com/harshil8981/TG-CONVERT-HPBOT/blob/main/LICENSE)
+
+## Status
+[![Website shields.io](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](https://github.com/harshil8981/TG-CONVERT-HPBOT/tree/main)
+![Forks](https://img.shields.io/github/forks/harshil8981/TG-CONVERT-HPBOT)
+![1](https://github-readme-stats.vercel.app/api/top-langs/?username=TG-CONVERT-HPBOT&theme=blue-green)
+[![Anonymous's github stats](https://github-readme-stats.vercel.app/api?username=TG-CONVERT-HPBOT&theme=blue-green)](https://github.com/anuraghazra/github-readme-stats)
+
+
+## Thanks and credits
+[![saythanks](https://img.shields.io/badge/say-thanks-ff69b4.svg)](https://saythanks.io/to/kennethreitz)
+- [Mahesh Malekar](https://telegram.dog/MaheshMalekar) for fixing some bugs
+- [Dan](https://telegram.dog/haskell) for his pyrogram docs
+
---
+++ translation.py
@@ -0,0 +1,102 @@
+class Translation(object):
+
+
+#This will be appeared when anyone use start command
+
+      START = """Hello {0}
+
+I am a converter clone of [Convert HP Bot](https://telegram.dog/Hp_converterbot) by {1}
+
+I can convert file to video or video to file with custom thumbnail support.
+"""
+
+
+#This will be appeared when anyone use help command
+
+      HELP = """**Hey you need help 🤔 ?**
+
+1. Send me the telegram file or video which you wanted to convert.
+
+2. Send me the thumbnail(photo) optional.
+
+3. Reply to video /converttofile for converting into file.
+
+4. Reply to file /converttovideo for converting into video.
+
+**SUPPORT GROUP:** [HP Bot Supporters](https://telegram.dog/Hp_botupdate)
+"""
+
+
+#Please don't change this about command 🙏
+
+      ABOUT = """
+**📝 Language:** Python 3
+
+**🧰 Framework:** Pyrogram
+
+**👨‍💻 Developer:** [Mr Killer](https://t.me/Mrkiller_1109)
+
+**📮 Channel:** [HP BOT UPDATES](https://t.me/Hp_botupdate)
+
+**👥 Group:** [HP BOT SUPPOTERS](https://t.me/HP_Bot_discuss_group)
+
+**💻 Source Code:**[Press Me](https://github.com/harshil8981/TG-CONVERT-HPBOT)
+
+"""
+
+####################################################################################################################################################
+####################################################################################################################################################
+
+
+
+#If you set the password for the bot if anyone use the bot without logging in this text will appear
+
+      NOT_LOGGED_TEXT = """ This bot was a private bot you need to login using the password.
+For logging in use command <code>/login BotPassword</code>. And then use me 🥰"""
+
+
+#This will be sent to the user when the user logged successfully
+
+      SUCESS_LOGIN = """You are successfully logged in. So you can use me for today.
+You access will be revoke by tomorrow"""
+
+
+# This will be show when an user send wrong password
+
+      WRONG_PWD = """This is a wrong password 🔐 please try with correct password"""
+
+
+# This will appear if the user is already logged
+
+      EXISTING_USER = "You are already logged in you can use me"
+
+####################################################################################################################################################
+####################################################################################################################################################
+
+
+#DON'T CHANGE THE NUMBERS IN THE FLOWER BRACKETS AND THE ORDER OF PERCENTAGE, DONE, TOTAL, SPEED, ETA ONLY CHANGE THE THEME 
+
+      PROGRESS = """
+Percentage : {0}%
+Done ✅: {1}
+Total 🌀: {2}
+Speed 🚀: {3}/s
+ETA 🕰: {4}
+"""
+       
+      DOWNLOAD_PROGRESS = "▪️"
+      UPLOAD_PROGRESS = "▫️"
+
+####################################################################################################################################################
+####################################################################################################################################################
+
+
+
+      DOWNLOAD_START = "Trying to Download 📥"
+      DOWNLOAD_COMPLETE = "✅ Media Downloaded successfully\nPreparing for upload"
+      UPLOAD_START = "Trying to Upload 📤"
+      UPLOAD_COMPLETE = "THANKS FOR USING ME"
+      SAVED_CUSTOM_THUMB_NAIL = "✅ Saved Thumbnail Successfully. This will be deleted in 24hrs"
+      BANNED_TEXT = "YOU ARE BANNED. SO YOUR ARE NOT ABLE TO USE ME 🐒"
+      REPLY_TEXT = "👩‍✈️ Reply to the media which you need to convert"
+      DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Deleted Successfully ✅"
